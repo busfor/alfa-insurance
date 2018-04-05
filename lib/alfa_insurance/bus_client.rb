bd2aae9c2aa886a4617839b4a6a3e1ea2978e471
Alexander Sviridov <alexander.sviridov@gmail.com>
2018-04-05 12:18:50 +0300

    Add ticket issue date param (mandatory in some environments)

--- lib/alfa_insurance/bus_client.rb
+++ lib/alfa_insurance/bus_client.rb
@@ -1,24 +1,25 @@
 module AlfaInsurance
   class BusClient < BaseClient
-    def calculate(total_cost)
+    def calculate(total_cost, ticket_issue_date)
       response = send_soap_request(:calculate_policy) do |xml|
         xml.operator { xml.code(operator) }
         xml.product { xml.code(product_code) }
         xml.policyParameters {
           xml.ticketInformation {
             xml.ticketTotalValue(total_cost.to_f)
+            xml.ticketIssueDate(ticket_issue_date.iso8601)
           }
         }
       end
       CalculateResponse.new(response)
     end
 
-    def create(insurance_object)
+    def create(insurance_object, ticket_issue_date)
       raise ArgumentError, "BusInsuranceRequest is expected" unless insurance_object.is_a?(BusInsuranceRequest)
       response = send_soap_request(:create_policy) do |xml|
         xml.operator { xml.code(operator) }
         xml.product { xml.code(product_code) }
-        xml.policyParameters { insurance_object.generate_xml(xml) }
+        xml.policyParameters { insurance_object.generate_xml(xml, ticket_issue_date) }
       end
       CreateResponse.new(response)
     end
