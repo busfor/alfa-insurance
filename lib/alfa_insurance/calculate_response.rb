f776fc9e10eac21214a564eb0b85de764401b943
Alexander Kashmatov <a.kashmatov@pm.me>
2019-08-28 19:51:37 +0300

    moved base code to parent class

--- lib/alfa_insurance/calculate_response.rb
+++ lib/alfa_insurance/calculate_response.rb
@@ -13,15 +13,30 @@ module AlfaInsurance
       risk_types.first
     end
 
-    def risk_types
-      risk_values.keys
-    end
-
     def risk_values
-      @risk_values ||= risk_values_from(result, currency: currency)
+      @risk_values ||= risk_values_from(result)
     end
 
-  private
+    def risk_types
+      @risk_types ||= begin
+        raw_values = result[:risk_value]
+        raw_values = [raw_values] unless raw_values.is_a?(Array)
+        raw_values.map { |raw_value| raw_value[:@risk_type] }
+      end
+    end
+
+    private
+
+    def risk_values_from(data)
+      raw_values = [data[:risk_value]].flatten
+      raw_currencies = [data[:risk_currency]].flatten
+
+      risk_types.each_with_object({}) do |risk_type, result|
+        value = raw_values.find { |raw| raw[:@risk_type] == risk_type }.fetch(:@value)
+        currency = raw_currencies.find { |raw| raw[:@risk_type] == risk_type }.fetch(:@value)
+        result[risk_type] = to_money(value, currency)
+      end
+    end
 
     def currency
       result[:currency]
--- lib/alfa_insurance/find_response.rb
+++ lib/alfa_insurance/find_response.rb
@@ -1,15 +1,15 @@
 module AlfaInsurance
   class FindResponse < CalculateResponse
     def insurance_id
-      policy[:policy_id].to_i
+      result[:policy_id].to_i
     end
 
     def state
-      policy[:policy_status]
+      result[:policy_status]
     end
 
     def cost
-      @cost ||= to_money(policy[:rate], policy[:currency])
+      @cost ||= to_money(result[:rate], result[:currency])
     end
 
     def risk_type
@@ -17,34 +17,14 @@ module AlfaInsurance
       risk_types.first
     end
 
-    def risk_types
-      [policy[:risk_list]].flatten
-    end
-
     def risk_value
       @risk_value ||= risk_values.values.inject(&:+)
     end
 
-    def risk_values
-      @risk_values ||= risks_from_raw(policy)
-    end
-
     private
 
-    def risks_from_raw(data)
-      risk_types = [data[:risk_list]].flatten
-      raw_values = [data[:risk_value]].flatten
-      raw_currencies = [data[:risk_currency]].flatten
-
-      risk_types.each_with_object({}) do |risk_type, result|
-        risk_value = raw_values.find { |raw| raw[:@risk_type] == risk_type }.fetch(:@value)
-        risk_currency = raw_currencies.find { |raw| raw[:@risk_type] == risk_type }.fetch(:@value)
-        result[risk_type] = to_money(risk_value, risk_currency)
-      end
-    end
-
-    def policy
-      @policy ||= body[:policy_information] || {}
+    def result
+      @result ||= body[:policy_information] || {}
     end
   end
 end
